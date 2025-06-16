b4477ba4e8a5afc56b88dc74fbf9b937b9bc6879
Achref23illi <achrefarabi414@gmail.com>
2025-06-16 15:13:29 +0200

    changes 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
       {/* Hero Carousel Section */}
       <HeroCarousel />
 
-      {/* Services Overview Section */}
+      {/* Services Overview Section  */}
       <ServicesOverview />
 
       {/* Stats Section */}

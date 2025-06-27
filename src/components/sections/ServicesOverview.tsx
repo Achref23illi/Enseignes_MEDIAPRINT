8c2f67687450fffa4e99969c9e7427508e2907fe
Achref23illi <achrefarabi414@gmail.com>
2025-06-27 12:11:00 +0200

    fix

--- src/components/sections/ServicesOverview.tsx
+++ src/components/sections/ServicesOverview.tsx
@@ -53,8 +53,8 @@ const ServicesOverview = () => {
         "Habillage mural et décoratif",
         "Graphismes architecturaux et de vitrines",
         "Matériaux extérieurs résistants aux intempéries"
-      ],
-      image: "/graphic_media/Habillage-CB1.jpg",
+      ], 
+      image: "/service2.jpg",
       icon: (
         <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
